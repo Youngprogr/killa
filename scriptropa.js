3577b3bc857d8439d4f4004afca11115717b3dcb
Igdoy25 <118281868+Youngprogr@users.noreply.github.com>
2024-01-12 01:03:49 -0500

    Add files via upload

--- scriptropa.js
+++ scriptropa.js
@@ -29,8 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
         rainContainer.appendChild(raindrop);
     }
 });
-
-
 //abrir menu//
 let menu=document.querySelector('#menu-icon');
 let navlist=document.querySelector('.navlist');
@@ -63,50 +61,3 @@ sr.reveal('.middle-text',{});
 sr.reveal('.row-btn,.shop-content',{delay:300});
 
 sr.reveal('.review,.contact',{delay:300});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const transitionLayer = document.getElementById("transition-layer");
-
-  // Muestra la capa de bienvenida
-  setTimeout(function () {
-      transitionLayer.classList.add("hide");
-  }, 2500); // Muestra la bienvenida durante 3 segundos
-
-  // Después de ocultar la capa de bienvenida, muestra el contenido de la página
-  setTimeout(function () {
-      transitionLayer.style.display = "none"; // Oculta la capa de bienvenida
-      document.body.style.overflow = "visible"; // Permite el desplazamiento
-  }, 5000); // Ajusta según sea necesario (3 segundos de bienvenida + 2 segundos de rotación)
-});
-
-
-
-function openWhatsApp(product) {
-    // Reemplaza con tu número de teléfono
-    var phoneNumber = '+51958784348';
-
-    // Construye el mensaje con el nombre del producto
-    var message = encodeURIComponent(`Hola estimado, me gustaría comprar la prenda ${product}.`);
-
-    // Construye el enlace de WhatsApp
-    var whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
-
-    // Abre una nueva ventana para iniciar la conversación en WhatsApp
-    var whatsappWindow = window.open(whatsappUrl, '_blank');
-
-    // Agrega un mensaje personalizado después de abrir la ventana
-    if (whatsappWindow) {
-        window.alert(`¡Hola! La conversación de WhatsApp se ha iniciado para comprar ${product}. Puedes continuar la conversación allí.`);
-    } else {
-        window.alert('No se pudo abrir la conversación de WhatsApp. Asegúrate de tener la aplicación instalada.');
-    }
-}
-
-function openWhatsApp() {
-    var phoneNumber = '+51958784348';
-    var message = encodeURIComponent('¡Hola! ¿En qué podemos ayudarte?');
-    var whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
-    window.open(whatsappUrl, '_blank');
-}
-
-
